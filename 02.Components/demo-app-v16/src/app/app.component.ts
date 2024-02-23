7ac83691168de1ae098cc73f576b074b81eb11e2
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-02-23 19:30:25 +0200

    update

--- 02.Components/demo-app-v16/src/app/app.component.ts
+++ 02.Components/demo-app-v16/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-app-v16';
+
+  colorValueForPlayground = 'beige';
 }
--- 02.Components/demo-app-v16/src/app/playground/playground.component.ts
+++ 02.Components/demo-app-v16/src/app/playground/playground.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent implements OnInit, OnDestroy{
+
+  @Input('color') colorValue='white';
+
+
   isToggle = false;
   badCurly='bad-curly';
   imgUrl = 'https://hips.hearstapps.com/hmg-prod/images/still-life-image-of-beautiful-pink-orchid-flowers-royalty-free-image-1702918746.jpg?crop=0.668xw:1.00xh;0.127xw,0&resize=340:*'
+
+
+  // Lifecycle hooks 
+  ngOnInit(){
+    console.log('Component Created!');
+  }
+
+   ngAfterViewInit(){
+    console.log('After Init');
+   }
+
+   ngOnDestroy(){
+    console.log('On destroy');
+    // clean up 
+   }
+
 
   handleClick() {
     console.log('clicked');
