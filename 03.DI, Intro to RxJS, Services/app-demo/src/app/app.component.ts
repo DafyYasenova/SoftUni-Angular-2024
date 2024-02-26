a597918b6942e16f903d3d9c8bb22304e5280704
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-02-26 14:04:45 +0200

    add users list

--- 03.DI, Intro to RxJS, Services/app-demo/src/app/app.component.ts
+++ 03.DI, Intro to RxJS, Services/app-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './type/User';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-demo';
+
+  users: User[] = [
+    { name: "Ani", age: 12 },
+    { name: "Mimi", age: 23 },
+    { name: "Toni", age: 22 },
+    { name: "Ani", age: 12 },
+    { name: "Bibi", age: 18 },
+
+  ];
+
+
 }
