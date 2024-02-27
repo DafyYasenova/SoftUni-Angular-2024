a38766e7bd9584dac251da41c9fe679e1b79ab04
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-02-27 16:04:11 +0200

    change detected

--- 03.DI, Intro to RxJS, Services/app-demo/src/app/app.component.ts
+++ 03.DI, Intro to RxJS, Services/app-demo/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { User } from './type/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app-demo';
@@ -18,10 +18,15 @@ export class AppComponent {
   //   }, 3000);
   // }
 
-constructor() {
-  setTimeout(() =>{
-    this.title = 'Change from Angular!';
-  }, 2000)
+constructor(private cd: ChangeDetectorRef) {
+  // setTimeout(() =>{
+  //   this.title = 'Change from Angular!';
+  // }, 2000)
+
+  setInterval(() => {
+    this.cd.detectChanges();
+    console.log('Changes Detected');
+  },3000);
 }
   users: User[] = [
     { name: "Ani", age: 12 },
@@ -32,6 +37,19 @@ constructor() {
 
   ];
 
+addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement){
 
+const user: User = {
+  name:inputName.value,
+  age: Number(inputAge.value),
+}
+  this.users.push(user); //push, but not update
+  // this.users = [...this.users, user]; // update ref
+  console.log(this.users);
+
+  // clear input:
+  inputName.value='';
+  inputAge.value='';
+}
 
 }
--- 03.DI, Intro to RxJS, Services/app-demo/src/app/users-list/users-list.component.ts
+++ 03.DI, Intro to RxJS, Services/app-demo/src/app/users-list/users-list.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { User } from '../type/User';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
+
+
 })
 export class UsersListComponent {
 @Input('users') usersListData: User[] = [];
+
+
+
+constructor(private cd: ChangeDetectorRef) {
+
+  setInterval(() => {
+    this.cd.detectChanges();
+    console.log('Changes Detected');
+  },3000);
+}
 }
