3cb09fcf2c2fc813e4e6b42578b00a11f0337a45
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-02-28 20:56:37 +0200

    add articles component

--- 02.Components/articles-app/src/app/data/data.ts
+++ 02.Components/articles-app/src/app/data/data.ts
@@ -7,7 +7,11 @@ export class ArticleData {
         let articles: Article[] = [];
 
         for (let i = 0; i < data.length; i++) {
-            articles[i] = new Article(data[i].title, data[i].description, data[i].author, data[i].imageUrl);
+            articles[i] = new Article(
+                data[i].title, 
+                data[i].description, 
+                data[i].author, 
+                data[i].imageUrl);
         }
         
         return articles;
