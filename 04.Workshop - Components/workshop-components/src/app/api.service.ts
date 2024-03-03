93b5632d16617f0e3ed7a63ac29cef606b1e0130
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-03 18:12:13 +0200

    add interface post

--- 04.Workshop - Components/workshop-components/src/app/api.service.ts
+++ 04.Workshop - Components/workshop-components/src/app/api.service.ts
@@ -10,12 +10,13 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getThemes (){
-const { apiURL } = environment;
-return this.http.get<Theme[]>(`${apiURL}/themes`);
+  getThemes() {
+    const { apiURL } = environment;
+    return this.http.get<Theme[]>(`${apiURL}/themes`);
   }
 
-  getPosts (limit?: number){
-
+  getPosts(limit?: number) {
+const { apiURL} = environment;
+return this.http.get<any>(`${apiURL}/posts`);
   }
 }
