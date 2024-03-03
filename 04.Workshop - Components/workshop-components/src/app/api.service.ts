e237cdaa1eb87882a05bd1032369e33a4fd1cab7
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-03 17:53:00 +0200

    add

---
+++ 04.Workshop - Components/workshop-components/src/app/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/theme';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getThemes (){
+const { apiURL } = environment;
+return this.http.get<Theme[]>(`${apiURL}/themes`);
+  }
+
+  getPosts (limit?: number){
+
+  }
+}
