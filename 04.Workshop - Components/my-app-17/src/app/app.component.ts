508a2a03317263ed52ca540310c3504a279d89bc
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-04 20:40:46 +0200

    update

--- 04.Workshop - Components/my-app-17/src/app/app.component.ts
+++ 04.Workshop - Components/my-app-17/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app-17';
+
+  // tasks1 =  { id:1, name: 'Homework1', status: 'done'};
+  // tasks2 =  { id:1, name: 'Homework1', status: 'done'};
 
   tasks = [
     { id:1, name: 'Homework1', status: 'done'},
