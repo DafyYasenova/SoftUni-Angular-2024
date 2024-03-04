0e8e7574d684682f5f0bec396bb37a5916304bba
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-04 20:12:18 +0200

    use control flou

--- 04.Workshop - Components/my-app-17/src/app/app.component.ts
+++ 04.Workshop - Components/my-app-17/src/app/app.component.ts
@@ -10,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-app-17';
+
+  tasks = [
+    { id:1, name: 'Homework1', status: 'done'},
+    { id:2, name: 'Homework2', status: 'todo'},
+    { id:3, name: 'Homework3', status: 'inProgress'},
+    { id:4, name: 'Homework4', status: 'inProgress'},
+  ]
 }
