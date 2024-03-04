bde2a05382e4a8598058143351770c19af1e871e
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-04 15:09:02 +0200

    rendering posts

--- 04.Workshop - Components/workshop-components/src/app/posts-list/posts-list.component.ts
+++ 04.Workshop - Components/workshop-components/src/app/posts-list/posts-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/post';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent implements OnInit{
-constructor(private api:ApiService){}
+export class PostsListComponent implements OnInit {
 
-ngOnInit(): void {
-  this.api.getPosts(5).subscribe((posts) =>{
-    console.log(posts);
-  })
-}
+  posts: Post[] = []
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getPosts(5).subscribe((posts) => {
+      console.log(posts);
+      this.posts = posts;
+    })
+  }
 }
