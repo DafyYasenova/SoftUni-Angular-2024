5090185c26977f2732454b60d9338592998ce9a1
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-09 14:37:11 +0200

    update

--- 05,06.Workshop- Modules and Routing/workshop-components/src/app/user/login/login.component.ts
+++ 05,06.Workshop- Modules and Routing/workshop-components/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+constructor (private userService: UserService){}
 
+login(event: Event, email: string, password:string){
+  event.preventDefault()
+  this.userService.login();
+}
 }
