4de476b011162b9272d7cf90fbf035695165e9e0
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-09 11:09:35 +0200

    themes routing

--- 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
+++ 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-// import { MainComponent } from "./main/main.component";
+import { MainComponent } from "./main/main.component";
+import { AddThemeComponent } from "./add-theme/add-theme.component";
+import { CurrentThemeComponent } from "./current-theme/current-theme.component";
 
-// const routes: Routes= [{path: 'themes', component: MainComponent}];
-const routes: Routes= [];
+const routes: Routes = [{
+    path: 'themes', children: [
+        { path: '', pathMatch: 'full', component: MainComponent },
+        { path: ':themeId', component: CurrentThemeComponent }
+    ],
+},
+{ path: 'add-theme', component: AddThemeComponent }];
+// const routes: Routes = [];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule],
 })
 
-export class ThemeRoutingModule {}
+export class ThemeRoutingModule { }
