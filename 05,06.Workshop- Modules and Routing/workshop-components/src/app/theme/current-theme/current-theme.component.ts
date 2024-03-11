e744ce1377923b3205fcfe0539f52d977c045153
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-11 11:43:49 +0200

    add current theme data

--- 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/current-theme/current-theme.component.ts
+++ 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/current-theme/current-theme.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/app/types/theme';
 
 @Component({
   selector: 'app-current-theme',
@@ -7,11 +8,16 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./current-theme.component.css']
 })
 export class CurrentThemeComponent implements OnInit{
+
+  theme = { } as Theme;
 constructor(private apiService: ApiService){}
 
   ngOnInit(): void {
-    this.apiService.getTheme('5fa64a9f2183ce1728ff371a').subscribe((data) => {
-      console.log(data)
+    this.apiService.getTheme('5fa64a9f2183ce1728ff371a').subscribe((theme) => {
+     
+      this.theme = theme;
+
+      console.log({theme})
     })
   }
 }
