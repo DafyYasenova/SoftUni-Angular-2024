0ee1a1295978841862bb3e2ac74bab9761f35f05
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-11 10:12:31 +0200

    add route guard

--- 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
+++ 05,06.Workshop- Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "./main/main.component";
 import { AddThemeComponent } from "./add-theme/add-theme.component";
 import { CurrentThemeComponent } from "./current-theme/current-theme.component";
+import AuthActivate from "../guards/auth.activate";
 
 const routes: Routes = [{
     path: 'themes', children: [
@@ -10,7 +11,7 @@ const routes: Routes = [{
         { path: ':themeId', component: CurrentThemeComponent }
     ],
 },
-{ path: 'add-theme', component: AddThemeComponent }];
+{ path: 'add-theme', component: AddThemeComponent , canActivate: [AuthActivate]}];
 // const routes: Routes = [];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
