6af27a20c62745ca96081544aeca3c95b10eba0b
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-12 10:26:25 +0200

    update higthligth

--- 07.Directives and Forms/directives-and-forms/src/app/highlight.directive.ts
+++ 07.Directives and Forms/directives-and-forms/src/app/highlight.directive.ts
@@ -8,16 +8,43 @@ export class HighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    console.log(this.elRef.nativeElement);
-
+    // console.log(this.elRef.nativeElement);
 
     // Bad practice:
-    this.elRef.nativeElement.style.background= 'pink';
-
+    // this.elRef.nativeElement.style.background= 'pink';
 
     // Good practice:
-    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'aquamarine');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'aquamarine');
 
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mouseEnterHandler.bind(this),
+    );
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler.bind(this))
   }
 
+
+  mouseEnterHandler(e: MouseEvent): void {
+    // console.log(e);
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'orange',
+    );
+
+  }
+  mouseLeaveHandler(e: MouseEvent): void {
+    // console.log('mouse leave');
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'grey',
+    )
+  };
 }
