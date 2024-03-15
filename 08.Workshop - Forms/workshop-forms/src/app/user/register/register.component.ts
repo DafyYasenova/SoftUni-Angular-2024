676685763b09cd947a959c635d7c14a44d3ae153
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-15 21:06:52 +0200

    reactive form register

--- 08.Workshop - Forms/workshop-forms/src/app/user/register/register.component.ts
+++ 08.Workshop - Forms/workshop-forms/src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+
+  form = this.fb.group({
+
+    username: ['dummy'],
+    email:['dummy"abv.bg'],
+    tel:['123'],
+    passGroup: this.fb.group({ password: ['123456'],
+    rePassword: ['1234'],}),
+   
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  register(): void {
+    if(this.form.invalid){
+      return;
+    }
+
+    console.log(this.form.value);
+  }
 }
--- 08.Workshop - Forms/workshop-forms/src/app/user/user.module.ts
+++ 08.Workshop - Forms/workshop-forms/src/app/user/user.module.ts
@@ -5,7 +5,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -22,6 +22,7 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule,
     FormsModule,
     SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
