351421ff2ca8dda52271498cf8536c352b184b80
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-17 11:59:52 +0200

    add edit functionality for prifile component

--- 08.Workshop - Forms/workshop-forms/src/app/user/profile/profile.component.ts
+++ 08.Workshop - Forms/workshop-forms/src/app/user/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
 import { ProfileDetails } from 'src/app/types/user';
 
 @Component({
@@ -8,20 +11,37 @@ import { ProfileDetails } from 'src/app/types/user';
 })
 export class ProfileComponent {
 
-  showEditMode: boolean= false;
+  showEditMode: boolean = false;
 
 
   profileDetails: ProfileDetails = {
     username: 'John Doe',
-    email:'johndoe@abv.bg',
+    email: 'johndoe@abv.bg',
     tel: '123-456',
   };
-  onEdit():void{
-    this.showEditMode = true;
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+    tel: [''],
+  })
+  constructor(private fb: FormBuilder) { }
+
+  onToggle(): void {
+    this.showEditMode = !this.showEditMode;
   }
 
 
+saveProfileHandler(): void{
 
+  console.log('invoked data', this.form.value)
+  if(this.form.invalid){
+    return;
+  }
+  
+  this.profileDetails = this.form.value as ProfileDetails;
+  this.onToggle();
+}
 
 
 }
