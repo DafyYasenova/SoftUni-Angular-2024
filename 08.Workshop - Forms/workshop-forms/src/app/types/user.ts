d54c7983b69d10b510d17898ff87d1891016ad6c
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-03-17 11:37:53 +0200

    add profile details interface

--- 08.Workshop - Forms/workshop-forms/src/app/types/user.ts
+++ 08.Workshop - Forms/workshop-forms/src/app/types/user.ts
@@ -20,3 +20,9 @@ export interface UserForAuth {
     password: string,
     id: string;
 }
+
+export interface ProfileDetails {
+    username: string,
+    email:string,
+    tel:string,
+}
--- 08.Workshop - Forms/workshop-forms/src/app/user/profile/profile.component.ts
+++ 08.Workshop - Forms/workshop-forms/src/app/user/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProfileDetails } from 'src/app/types/user';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  showEditMode: boolean= false;
+
+
+  profileDetails: ProfileDetails = {
+    username: 'John Doe',
+    email:'johndoe@abv.bg',
+    tel: '123-456',
+  };
+  onEdit():void{
+    this.showEditMode = true;
+  }
+
+
+
+
 
 }
